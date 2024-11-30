e9e793a03920e1cf38ed5725287d3e8e888385d4
Andrew Latimer <amlatimer@ucdavis.edu>
2024-11-30 13:12:32 -0800

    bunch of small comment and variable name improvements for clarity

--- scripts/daily_gridmet_data_example.R
+++ scripts/daily_gridmet_data_example.R
@@ -21,90 +21,109 @@ PLSS_DIR = "~/Library/CloudStorage/Box-Box/climate-data-workshop/plss_data"
 ## Many climate layers, including daily gridmet data, are normally stored in compact nbinary format called netCDF 
 
 # Download netcdf file from the THREDDs climate data server
+# see http://thredds.northwestknowledge.net:8080/thredds/reacch_climate_MET_catalog.html 
 
-# First set the timeout option to a longer time to make sure that the download function has enough time to get the whole file 
+# First we might need to set the "timeout option" to a longer time to make sure that the download function doesn't just give up before getting the whole file 
 options(timeout = max(300, getOption("timeout"))) 
 
 # Download the netCDF file for one year of daily maximum temperature data
-downloader::download(url = str_c("http://www.northwestknowledge.net/metdata/data/tmmx_2023.nc"), destfile = paste0(CLIM_DIR, "/tmmx_2023.nc"), mode = "wb")
+#downloader::download(url = str_c("http://www.northwestknowledge.net/metdata/data/tmmx_2023.nc"), destfile = paste0(CLIM_DIR, "/tmmx_2022.nc"), mode = "wb")
 
-# Load the netCDF file as one big, multilayer raster
+# Load a netCDF file of daily data for one year as one big, multilayer raster
 list.files(CLIM_DIR)
-tmax <- rast(paste(CLIM_DIR, list.files(CLIM_DIR)[1], sep = "/"))
-head(names(tmax))
+r <- rast(paste0(CLIM_DIR, "/tmmx_2023.nc"))
+dim(r)
 
 # Plot the first few layers of the raster
-plot(tmax[[1:9]])
+plot(r[[1:9]])
 
 # What is the name of the layers?
-names(tmax)[1:9]
-
-# Join these columns onto the plot data. The order of the rows in the extracted data is the same as
-# the order of the plot data.
-plots = cbind(plots, extracted)
-head(plots)
+names(r)[1:9]
 
 
-#### Loading, looking, at, finding centroids of spatial feature objects  
+# Now we can extract data from the raster using spatial features (points, polygons)
+
+# One source of data might be a field GPS, like Derek's example last week. 
+
+# Another source is polygons from a map such as fire perimeters or PLSS data. Using PLSS data as an example, let's try using centroids of spatial feature polygons to extract data.   
 
 # Load PLSS grid for CA 
 plss_grid <- st_read(paste0(PLSS_DIR, "/plss_grid.gpkg"))
+
+# Check out some random set of grid cells 
 ggplot(plss_grid[1:2000,]) + geom_sf()
 
+# Scenario 1: we have a few specific TRS grid cells in mind to look at. 
 # Subset this grid -- for now using some arbitrary locations 
-plss_grid_subset = plss_grid |> dplyr::filter(MTRS %in% c("MDM-T15N-R10E-15", "MDM-T15N-R15E-15", "MDM-T20N-R10E-15", "MDM-T20N-R15E-15"))
-plot(plss_grid_subset[1])
+plss_grid_subset = plss_grid |> dplyr::filter(MTRS %in% c("MDM-T15N-R10E-15", "MDM-T18N-R12E-15", "MDM-T20N-R10E-15", "MDM-T22N-R15E-15"))
+ggplot(plss_grid_subset) + geom_sf()
 
-# More usefully, maybe, we could subset the grid based on a single seed zone and elevation band combination 
+# Scenario 2: We could also subset the grid based on a region like a single seed zone. 
+
+# Load the USFS seed zones layer
 seed_zones <- st_read(paste(PLSS_DIR, "seed_zones.gpkg", sep = "/"))
+
+# Transform the seed zones to the same projection as the plss grid 
+seed_zones = st_transform(seed_zones, st_crs(plss_grid))
+
 # Take a look at one or more seed zones 
 seed_zones |> 
   filter(SEED_ZONE == "531") |> 
   ggplot() + geom_sf()
-# Now grab the PLSS grid cells that intersect with this seed zone 
-# Transform the seed zones to the same projection as the plss grid 
-seed_zones = st_transform(seed_zones, st_crs(plss_grid))
+
+# Now grab the PLSS grid cells that intersect with this seed zone
 plss_grid_subset = st_intersection(plss_grid, seed_zones[seed_zones$SEED_ZONE == 531,])
 ggplot(plss_grid_subset) + geom_sf()
 
-# Get the centroid of our subset of TRS grid squares
-
-focal_centroids <- plss_grid_subset |> st_centroid()
+# Get the centroids of our subset of TRS grid squares
+focal_centroids = plss_grid_subset |> st_centroid()
 
 # Extract the value of the climate raster at the centroid of the PLSS grid
-extracted <- terra::extract(tmax, focal_centroids, method = "bilinear")
+extracted = terra::extract(r, focal_centroids, method = "bilinear")
 
-# Combine the extracted data with the PLSS grid data
-d = cbind(plss_grid_subset, extracted)
+# Add the site/grid information to the extracted data to keep it all together
+d = cbind(focal_centroids, extracted)
 
-# Optionally remove the geospatial data
-#d = st_drop_geometry(plots)
-#head(d)
+# Now we have the climate data for our plots in a tabular format! Now let's do some tabular data wrangling.
 
-# Now we have the climate data for our plots in a tabluar format! Now let's do some tabular data
-# wrangling.
+names(d) # data are very wide because there's a column for each day! 
 
 # Convert the data from wide to long data format 
-d_long = pivot_longer(d, cols = starts_with("air_"), names_to = "gridmet_layer", values_to = "value")
+d_long = pivot_longer(d, cols = starts_with("air_"), names_to = "climate_layer", values_to = "value")
+names(d_long)
 
-# Split the label column into info about the variable and the date
+# Check out the "climate_layer" variable 
+d_long$climate_layer[1:10]
+
+# Split this column into info about the variable (air_temperature_day) and the date (days since 1/1/1900)
 d_long = d_long  |>
-  separate_wider_delim(gridmet_layer, names = c("clim_var", "day_1900"), delim = ".")
-head(d_long)
+  separate_wider_delim(climate_layer, names = c("clim_var", "days_since_1900"), delim = ".")
 
-# Convert the day format (years since 1/1/1900) to more interpretable date info
+d_long |> select(clim_var, days_since_1900) |> head()
+
+# Note that because this data set has a row for each day for each location, it's pretty big!
+dim(d_long)
+
+# Convert the "days_since_1900" date info and degrees K data to more understandable formats
 d_long = d_long  |>
-  mutate(date = lubridate::ymd("1900-01-01") + as.numeric(day_1900), tmax_c = value - 273.15) |> 
-  mutate(year = year(date), month = month(date), day = day(date), julian_date = lubridate::yday(date))
-head(d_long) 
+  mutate(date = lubridate::ymd("1900-01-01") + as.numeric(days_since_1900),
+        tmax_c = value - 273.15) |> 
+  mutate(year = year(date), month = month(date), day = day(date), 
+         julian_date = lubridate::yday(date))
 
-# Plot the time series
-ggplot(d_long, aes(x = date, y = tmax_c, group = ID, color = ID)) + geom_line() + labs(title = "Time series of tmax", x = "Date", y = "Temperature (C)") + theme_minimal()
+d_long |> select(days_since_1900, date, year, month, day, julian_date) |> head()
 
-# We could then define any kind of function we want to use to make summaries of the daily data -- such as climate indices
+# Plot the time series for a few locations
+ggplot(d_long |> filter(ID < 21), aes(x = date, y = tmax_c, group = ID, color = ID)) + geom_line() + labs(title = "Time series of tmax", x = "Date", y = "Temperature (C)") + theme_minimal()
+
+
+#### Converting daily time series data into annual weather indices 
+
+# We can define any kind of function we want!
 
 # Example: growing degree days
-calculate_gdd <- function(temp, base_temp) {
+calculate_gdd <- function(temp, base_temp = 10) {
+  if (is.null(temp)) stop("No temperature data provided")
   gdd = sum(pmax(temp - base_temp, 0))
   return(gdd)
 }
@@ -112,18 +131,12 @@ calculate_gdd <- function(temp, base_temp) {
 # Use the climate index function to summarize by year and point location
 d_annual <- d_long |> 
   group_by(year, ID) |> 
-  summarize(gdd = calculate_gdd(tmax_c, 10)) |>
+  summarize(gdd = calculate_gdd(tmax_c)) |>
   cbind(focal_centroids)
   
 # To visualize the results, plot growing degree days for the extracted points as a map
-ggplot(d_annual) + geom_sf(aes(geometry = geom, color = gdd)) + scale_color_viridis_c(option = "plasma") + labs(title = "Growing degree days", fill = "GDD")
+ggplot(d_annual) + geom_sf(aes(geometry = geom, color = gdd)) + scale_color_viridis_c(option = "plasma") + labs(title = "Growing degree days", fill = "GDD", x = "Longitude (decimal degrees E)", y = "Latitude (decimal degrees N)") + theme_minimal()
 
 
-################
-# To retrieve these we need to set the time period and the bounding box of the area we are interested in
-# For now let's just work with data from one year which means we only have to access a single netcdf file. We could automate accessing them all later!
-#clim_var <- "tmmx" # maximum temperature for example
-#clim_var_name <- "air_temperature"
-#clim_year <- 2023
-#bbox <- c(40, -120, -118,  39) # west, east, south, north
-#system(paste0("wget -P ", CLIM_DIR, " http://thredds.northwestknowledge.net:8080/thredds/ncss/MET/", clim_var, "/", clim_var, "_", clim_year, ".nc?var=", clim_var_name, "&north=", bbox[1], "&west=", bbox[2], "&east=", bbox[3], "&south=", bbox[4], "&disableProjSubset=on&horizStride=1&time_start=2024-01-01T00%3A00%3A00Z&time_end=2024-11-27T00%3A00%3A00Z&timeStride=1&accept=netcdf"))
+#### Next steps: we could automate downloading and extracting from netCDF files for many different years and multiple climate variables! 
+

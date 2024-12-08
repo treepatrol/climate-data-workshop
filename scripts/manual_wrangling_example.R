ba25ed79b2408c69f7b0a84fa77f6839afb1764a
jecribbs <jecribbs@ucdavis.edu>
2024-12-08 13:43:00 -0800

    All code worked with original data set. Added comments to check my understanding. Brainstormed thoughts about how to apply this to YOSE and SEKI in comments at the end.

--- scripts/manual_wrangling_example.R
+++ scripts/manual_wrangling_example.R
@@ -1,4 +1,8 @@
-# Script to demonstrate loading and summarizing monthly climate data layers
+# Author: Andrew Latimer
+# Date: Fall Quarter 2024
+# Input:
+# Code Description: Script to demonstrate loading and summarizing monthly climate data layers
+# Output: 
 
 library(tidyr)
 library(dplyr)
@@ -9,10 +13,10 @@ library(purrr)
 #### Constants
 
 # Path to the directory containing the monthly climate data layers
-CLIM_DIR = "~/Downloads/climate_workshop/prism_data_monthly"
+CLIM_DIR = "/Users/jennifercribbs/Documents/TreePatrol.org/Analysis/Data/prism_data_monthly"
 
-# Path to a file containing points to get cliate data for
-PLOTS_FILE = "~/Downloads/climate_workshop/plot_locs/ofo-plots.gpkg"
+# Path to a file containing points of interest
+PLOTS_FILE = "/Users/jennifercribbs/Documents/TreePatrol.org/Analysis/Data/plot_locs/ofo-plots.gpkg"
 
 
 #### Workflow
@@ -20,7 +24,8 @@ PLOTS_FILE = "~/Downloads/climate_workshop/plot_locs/ofo-plots.gpkg"
 ## Load the climate data layers
 
 # Get a list of all the relevant files
-rast_files = list.files(CLIM_DIR, pattern = "[0-9]{6}_bil.bil$")
+rast_files = list.files(CLIM_DIR, pattern = "[0-9]{6}_bil.bil$") # pattern gets any thing with 6 digits before _bil.bil
+# there are 6 plots/sites with 6 files each with a different file type that work together similar to a shapefile
 
 # Load them all as a big many-layer raster
 r = rast(paste0(CLIM_DIR, "/", rast_files))
@@ -28,22 +33,22 @@ r = rast(paste0(CLIM_DIR, "/", rast_files))
 ## Manually inspecting the layers
 
 # Example of viewing a single layer (i.e. one month)
-plot(r[[1]])
+plot(r[[7]]) # 7 is July
 
 # What is the name of that layer?
-names(r)[[1]]
+names(r)[[1]] # first layer is January
 
 # You could do the same thing for any of the layers. If you want to look up which layer to display,
 # you can display all the names:
 names(r)
 
 # Then you can plot layer(s) by name
-plot(r[["PRISM_tmean_stable_4kmM3_202210_bil"]])
+plot(r[["PRISM_tmean_stable_4kmM3_202210_bil"]]) # october
 
 ## Extracting values from the rasters at point (e.g., plot) locations
 
 # Load plot locations
-plots = st_read(PLOTS_FILE)
+plots = st_read(PLOTS_FILE) # 58 plots
 
 # Extract values from the rasters at the plot locations
 extracted = extract(r, plots, method = "bilinear")
@@ -66,10 +71,11 @@ get_5th = function(x) {
     unlist(fifth)
 }
 
+# names now corresponds to YYYYMM
 names(extracted) = get_5th(names(extracted))
 
-# Prepend these names with "ppt_
-names(extracted) = paste0("ppt_", names(extracted))
+# Prepend these names with tmean (or ppt_ etc)
+names(extracted) = paste0("tmean_", names(extracted))
 head(extracted)
 
 # Join these columns onto the plot data. The order of the rows in the extracted data is the same as
@@ -88,11 +94,11 @@ d = st_drop_geometry(plots)
 head(d)
 
 # For data summaries, some operations are easier in a "long" format
-d_long = pivot_longer(d, cols = starts_with("ppt_"), names_to = "prism_layer", values_to = "value")
+d_long = pivot_longer(d, cols = starts_with("tmean_"), names_to = "prism_layer", values_to = "value")
 
 head(d_long)
 
-# Now we will split the "ppt" and "YYYYMM" part of the column names into their own columns
+# Now we will split the "tmean" and "YYYYMM" part of the column names into their own columns
 d_long = d_long  |>
     separate(prism_layer, into = c("clim_var", "yearmonth"), sep = "_")
 head(d_long)
@@ -101,10 +107,10 @@ head(d_long)
 d_long = d_long  |>
     separate(yearmonth, into = c("year", "month"), sep = 4)
 
-# Now we have a nice way of operating on the monthly climate data for eacn plot. For example, we can
-# pull out precip for February 2020
+# Now we have a nice way of operating on the monthly climate data for each plot. For example, we can
+# pull out tmean for February 2020
 d_long |>
-    filter(clim_var == "ppt", year == "2020", month == "02")
+    filter(clim_var == "tmean", year == "2020", month == "02")
 
 # Or we can get annual summaries for each plot and climate variable
 annual_summary = d_long |>
@@ -112,3 +118,7 @@ annual_summary = d_long |>
     summarize(mean = mean(value, na.rm = TRUE),
               sd = sd(value, na.rm = TRUE))
 head(annual_summary)
+
+# May want to start with an annual summary for 2023 for YOSE and an average for 2022-2024 for SEKI
+# Could also consider the complex WPBR life cycle and think about when temp and precip are most relevant in this context. # Then we could pull specific months or seasons of interest in light of disease biology.
+# Finally, could look at climate as a time series variable, which might be more informative when thinking about multiple disturbances (e.g. cycles of drought and fire with potentially long lag effects)
